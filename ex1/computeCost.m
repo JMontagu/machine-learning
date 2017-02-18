6067f3687dff1671288c3b3c3a5d29ec6a4d095c
Jade Montagu <jade@jademontagu.com>
2017-02-18 23:02:48 +0000

    Compute cost, plot data

--- ex1/computeCost.m
+++ ex1/computeCost.m
@@ -13,7 +13,17 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
+% X = 
 
+h = X * theta;
+
+error = h - y;
+
+error_sqr = error.^2;
+
+q = sum(error_sqr);
+
+J = 1/(2*m)*q;
 
 
 
